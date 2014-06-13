c76ead83a03998f3e8291e99b9a0dd8c71427b3f
kmb32123 <breyk@uwplatt.edu>
2014-06-13 10:47:49 -0500

    Refactor
    Replace `Platteville` name with more generic `self.location`

--- Ayos.py
+++ Ayos.py
@@ -11,27 +11,27 @@ class AyosDaemon(Daemon):
     default_conf = '/etc/ayosdaemon.conf'
     section = 'ayos'
     
-    Platteville = ephem.Observer()
+    self.location = ephem.Observer()
     Sun = ephem.Sun()
 
     def run(self):
         relay = Relay(args.port)
-        Platteville.lat=args.latitude
-        Platteville.lon=args.longitude
+        self.location.lat=args.latitude
+        self.location.lon=args.longitude
 
         if nightTime()
             relay.turnOff()
             
-            sleepUntil(Platteville.next_rising(Sun).datetime())
+            sleepUntil(self.location.next_rising(Sun).datetime())
 
         while True:
             relay.turnOn()
 
-            sleepUntil(Platteville.next_setting(Sun).datetime())
+            sleepUntil(self.location.next_setting(Sun).datetime())
             
             relay.turnOff()
 
-            sleepUntil(Platteville.next_rising(Sun).datetime())
+            sleepUntil(self.location.next_rising(Sun).datetime())
             
     def add_arguments(self):
         super(AyosDaemon, self).add_arguments()
@@ -44,7 +44,7 @@ class AyosDaemon(Daemon):
         self.parser.description = 'Run Ayos light controller for a specified location'
         
     def nightTime(self):
-        return Platteville.next_rising(Sun).datetime() < Platteville.next_setting(Sun).datetime()
+        return self.location.next_rising(Sun).datetime() < self.location.next_setting(Sun).datetime()
         
     def sleepUntil(self, date):
         time.sleep(date - datetime.utcnow())
